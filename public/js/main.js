e6735360a68479f98e47da0549fb6baccd875931
cjjeakle <cjjeakle@gmail.com>
2013-06-22 22:19:38 -0400

    Updated navbar to be collapsable, avoided bulky libraries to keep site lightweight

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,48 @@
-$(function() {
+$(function()
+{
 
 });
+
+function findByClass(className, domNode, tagName) {
+	if (domNode == null)
+	{
+		domNode = document;
+	}
+	if (tagName == null)
+	{
+		tagName = '*';
+	}
+	var elts = new Array();
+	var tags = domNode.getElementsByTagName(tagName);
+	var findThis = " "+className+" ";
+	for(i = 0, j = 0; i < tags.length; i++) {
+		var test = " " + tags[i].className + " ";
+		if (test.indexOf(findThis) != -1)
+		{
+			elts[j++] = tags[i];
+		}
+	}
+	return elts;
+}
+
+var hidden = false;
+function toggleShow(obj)
+{
+	hidden = !hidden;
+	titles = findByClass('navHeader', null, null);
+	elts = findByClass(obj, null, 'div');
+	for (i = 0; i < elts.length; i++)
+	{	
+		if (hidden)
+		{
+			titles[i].innerHTML = "Navigation &#9658";
+		    elts[i].style.display = 'none';
+		}
+		else
+		{
+			titles[i].innerHTML = 'Navigation &#9660';
+		    elts[i].style.display = 'inherit';
+		}
+	}
+}
+
